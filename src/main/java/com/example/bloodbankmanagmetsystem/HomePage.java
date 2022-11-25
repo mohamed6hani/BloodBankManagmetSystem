4c328f723e89fb65b323f2c2a7d35b649ce51243
mohamed6hani <117268475+mohamed6hani@users.noreply.github.com>
2022-11-25 21:50:05 +0200

    Mohamed Hani

--- src/main/java/com/example/bloodbankmanagmetsystem/HomePage.java
+++ src/main/java/com/example/bloodbankmanagmetsystem/HomePage.java
@@ -17,10 +17,11 @@ public class HomePage extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+
         System.out.println("Done!");
         System.out.println("Trying2!");
         System.out.println("Done2!");
+        launch();
 
     }
 }
