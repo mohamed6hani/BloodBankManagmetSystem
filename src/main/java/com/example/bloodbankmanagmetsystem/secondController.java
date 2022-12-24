242095fcb2ed4ba45d365afe8eeb8ea31a56c586
mohamed6hani <117268475+mohamed6hani@users.noreply.github.com>
2022-12-24 19:47:41 +0200

    Mohamed Hani

--- src/main/java/com/example/bloodbankmanagmetsystem/secondController.java
+++ src/main/java/com/example/bloodbankmanagmetsystem/secondController.java
@@ -85,6 +85,14 @@ public class secondController implements Initializable {
         }
 
     }
+    public void switchToIdentify(ActionEvent event) throws IOException {
+        Parent fxmlLoader = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("IdentifyPage.fxml"))));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 
     public boolean validateLogin (){
 
