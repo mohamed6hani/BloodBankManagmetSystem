4a1f8af6ec7ab41abab65f7d3faa6b54cb94dba8
SalmaAhmed112 <sal.ali@nu.edu.eg>
2022-12-31 16:47:24 +0200

    add request

--- src/main/java/com/example/bloodbankmanagmetsystem/doctor.java
+++ src/main/java/com/example/bloodbankmanagmetsystem/doctor.java
@@ -1,5 +1,10 @@
 package com.example.bloodbankmanagmetsystem;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class doctor extends Person{
     doctor(String email, String password){
         super(email, password);
@@ -7,9 +12,20 @@ public class doctor extends Person{
     };
     private int DocID;
     private String hospital;
-    private String doctorlogin = "SELECT count(1) FROM Doctor WHERE Doctor_email = '" +this.email+ "' AND Doctor_password = '" +this.password+ "'";
+    private String doctorlogin = "SELECT count(1) FROM Doctor WHERE Doctor_email = '" +this.email+ "' AND Doctor_password = '" +this.password+ "'";};
 
-    public String getdoctorlogin(){
-        return doctorlogin;
-    };
-}
+public void verify_doc_Login(String dlogin){
+        DB db=new DB();
+        Connection
+        try{
+           con=db.getConnection();
+           Statement stmt=con.createStatement();
+           stmt.executeUpdate(sql:"INSERT INTO doctors(Doctor_id,Doctor_name,Doctor_email,Doctor_password,Doctor_Hospital,Doctor_phonenum");
+           }
+        catch(Exception e){
+        e.printStackTrace();
+        e.getCause();
+        }
+        }
+
+
