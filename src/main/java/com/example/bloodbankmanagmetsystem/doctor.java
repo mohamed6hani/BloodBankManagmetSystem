cb06a1a027c70480e9aae325a7cd02546a2f1144
SalmaAhmed112 <sal.ali@nu.edu.eg>
2022-12-31 19:34:38 +0200

    salma

--- src/main/java/com/example/bloodbankmanagmetsystem/doctor.java
+++ src/main/java/com/example/bloodbankmanagmetsystem/doctor.java
@@ -5,27 +5,33 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class doctor extends Person{
-    doctor(String email, String password){
+public class doctor extends Person {
+    doctor(String email, String password) {
         super(email, password);
 
-    };
+    }
+
+    ;
     private int DocID;
     private String hospital;
-    private String doctorlogin = "SELECT count(1) FROM Doctor WHERE Doctor_email = '" +this.email+ "' AND Doctor_password = '" +this.password+ "'";};
+    private String doctorlogin = "SELECT count(1) FROM Doctor WHERE Doctor_email = '" + this.email + "' AND Doctor_password = '" + this.password + "'";
+    public void verify_doc_Login(String dlogin){
 
-public void verify_doc_Login(String dlogin){
         DB db=new DB();
         Connection
         try{
-           con=db.getConnection();
-           Statement stmt=con.createStatement();
-           stmt.executeUpdate(sql:"INSERT INTO doctors(Doctor_id,Doctor_name,Doctor_email,Doctor_password,Doctor_Hospital,Doctor_phonenum");
-           }
+            con=db.getConnection();
+            Statement stmt=con.createStatement();
+            stmt.executeUpdate(sql:"INSERT INTO doctors(Doctor_id,Doctor_name,Doctor_email,Doctor_password,Doctor_Hospital,Doctor_phonenum");
+        }
         catch(Exception e){
-        e.printStackTrace();
-        e.getCause();
+            e.printStackTrace();
+            e.getCause();
         }
-        }
+    }
+}
+
+
+
 
 
